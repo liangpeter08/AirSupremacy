889312e6190e5e2adc3fc422f1733fdc52fba865
peter <ycpliang@edu.uwaterloo.ca>
2018-02-08 21:42:49 -0500

    intial Commit

---
+++ desktop/src/com/savory/airsupremacy/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.savory.airsupremacy.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.savory.airsupremacy.AirSupremacy;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new AirSupremacy(), config);
+	}
+}
